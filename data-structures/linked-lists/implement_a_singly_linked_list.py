98f6c4ee00d468090d47e97ab78714958ac4e4a2
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-03-20 09:47:53 -0300

    Singly Linked List

---
+++ data-structures/linked-lists/implement_a_singly_linked_list.py
@@ -0,0 +1,75 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+    def __repr__(self):
+        return self.data
+
+
+class MyLinkedList:
+    def __init__(self, value):
+        self.head = Node(value)
+        self.tail = self.head
+        self.length = 1
+
+    def __repr__(self):
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append(str(node.data))
+            node = node.next
+        nodes.append("None")
+        return " --> ".join(nodes)
+
+    def append(self, value):
+        new_node = Node(value)
+        self.tail.next = new_node
+        self.tail = new_node
+        self.length += 1
+
+    def prepend(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+        self.length += 1
+
+    def get(self, index):
+        return self.__traverse_to_index(index).data
+
+    def __traverse_to_index(self, index):
+        if index >= self.length:
+            return self.tail
+
+        i = 0
+        node = self.head
+        while i != index:
+            node = node.next
+            i += 1
+        return node
+
+    def insert(self, value, index):
+        node = self.__traverse_to_index(index - 1)
+        new_node = Node(value)
+        new_node.next = node.next
+        node.next = new_node
+        self.length += 1
+
+    def delete(self, index):
+        node = self.__traverse_to_index(index - 1)
+        node.next = node.next.next
+        self.length -= 1
+
+
+my_linked_list = MyLinkedList(10)
+my_linked_list.append(5)
+my_linked_list.prepend(16)
+my_linked_list.insert(3, 1)
+print(my_linked_list)
+print(my_linked_list.get(1))
+my_linked_list.delete(2)
+print(my_linked_list)
+my_linked_list.insert(200, 99999)
+print(my_linked_list)
+my_linked_list.delete(9999992828)
+print(my_linked_list)
